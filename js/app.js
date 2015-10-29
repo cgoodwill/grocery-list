20dfbcb535de09e4c9183a7c48673dd4c8b8f399
cgoodwill <christy.goodwill@gmail.com>
2015-10-29 11:47:02 -0600

    change itembutton and centering

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
   // alert('test');
-  $('.add-item').click(function() {
-    // alert('test');
-    var item = $('.new-item').val();
+  // $('.add-item').click(function() {
+  //   // alert('test');
+  //   var item = $('.new-item').val();
 
-    $('.new-item').val('');
-    $('.active-list').append('<li>' + item + '</li>');
+ 
+ 	// if (item) {
+  //   	$('.new-item').val('');
+  //   	$('.active-list').append('<li>' + item + '</li>');
+  //   }
+    
+  // });
+	
+  $('.form-item').submit(function(e) {
+
+  	// Prevents page from refreshing
+  	e.preventDefault();
+  	// alert('test');
+
+  	var item = $('.new-item').val();
+
+ 
+ 	if (item) {
+    	$('.new-item').val('');
+    	$('.active-list').append('<li>' + item + '</li>');
+    }
   });
 
   $('ul').on('click', 'li', function(event) {
